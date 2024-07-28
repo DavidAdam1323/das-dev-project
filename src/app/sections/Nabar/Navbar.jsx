a594d6c41f57c1ccb3152dc8dbac07994956c454
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-28 20:42:36 +0100

    Add animated hamburger menu icon.

--- src/app/sections/Nabar/Navbar.jsx
+++ src/app/sections/Nabar/Navbar.jsx
@@ -1,13 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./Navbar.module.css";
 import logo from "../../../../public/logos/DAS-Dev-logo2.svg";
+import { Spin as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
   return (
     <nav className={styles.navBarContainer}>
       <a href="#home">
-        <Image src={logo} alt="DAS.Dev Logo" width={150} height={100} />
+        <Image src={logo} alt="DAS.Dev Logo" width={200} height={150} />
       </a>
+      <Hamburger size={75} />
     </nav>
   );
 };
