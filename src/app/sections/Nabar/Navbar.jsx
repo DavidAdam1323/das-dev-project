04f78d5f153c00358212513816f96877dd875c49
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-29 09:54:08 +0100

    Add cropped SVG logo01, logo02.

--- src/app/sections/Nabar/Navbar.jsx
+++ src/app/sections/Nabar/Navbar.jsx
@@ -1,17 +1,49 @@
 "use client";
-
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./Navbar.module.css";
-import logo from "../../../../public/logos/DAS-Dev-logo2.svg";
+import logo from "../../../../public/logos/DAS-Dev-logo02.svg";
 import { Spin as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className={styles.navBarContainer}>
-      <a href="#home">
-        <Image src={logo} alt="DAS.Dev Logo" width={200} height={150} />
-      </a>
-      <Hamburger size={75} />
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <a href="#home">
+          <Image src={logo} alt="DAS.Dev Logo" width={200} height={48} />
+        </a>
+      </div>
+      <div className={styles.hamburgerContainer}>
+        <Hamburger toggled={isOpen} toggle={toggleMenu} size={75} />
+      </div>
+      <ul className={`${styles.navLinks} ${isOpen ? styles.navLinksOpen : ""}`}>
+        <li>
+          <a href="#home" onClick={toggleMenu}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="about" onClick={toggleMenu}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="project" onClick={toggleMenu}>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="contact" onClick={toggleMenu}>
+            Contact
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
