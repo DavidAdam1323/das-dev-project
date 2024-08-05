6dfca3233870ca5c66aeee056fb05832a50ccbc0
DavidAdam1323 <davidadamgtr@gmail.com>
2024-08-05 14:34:16 +0100

    Refactor Navbar component and CSS for responsiveness and performance improvements
    - Consolidated mobile and desktop navigation bars into a single component.
    - Added  attribute to logo images to improve Largest Contentful Paint (LCP) performance.
    - Standardized and cleaned up CSS styles for both mobile and desktop views.
    - Ensured responsive design using media queries.
    - Included icons for navigation links in both mobile and desktop views.
    - Improved overall code structure and readability.

--- src/app/sections/Nabar/Navbar.jsx
+++ src/app/sections/Nabar/Navbar.jsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import Image from "next/image";
 import styles from "./Navbar.module.css";
-import logo from "../../../../public/logos/DAS-Dev-logo02.svg";
 import { Spin as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
@@ -12,87 +11,118 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <nav className={styles.navBar}>
-      {/* Logo container */}
-      <div className={styles.logoContainer}>
-        <a href="#home">
-          <Image src={logo} alt="DAS.Dev Logo" width={190} height={40} />
-        </a>
+    <>
+      <div className={styles.navContainer}>
+        <nav className={styles.desktopNavBar}>
+          <div className={styles.logoContainer}>
+            <a href="#home">
+              <Image
+                src="/logos/DAS-Dev-logo02.svg"
+                alt="DAS.Dev Logo"
+                width={190}
+                height={40}
+                priority
+              />
+            </a>
+          </div>
+          <ul className={styles.desktopNavLinks}>
+            {["Home", "About", "Projects", "Contact"].map((text, index) => (
+              <li key={index}>
+                <a href={`#${text.toLowerCase()}`}>{text}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <nav className={styles.navBar}>
+          <div className={styles.logoContainer}>
+            <a href="#home">
+              <Image
+                src="/logos/DAS-Dev-logo02.svg"
+                alt="DAS.Dev Logo"
+                width={190}
+                height={40}
+                priority
+              />
+            </a>
+          </div>
+          <div className={styles.mobileNavBarContainer}>
+            <div className={styles.hamburgerContainer}>
+              <Hamburger
+                toggled={isOpen}
+                toggle={toggleMenu}
+                rounded
+                distance="md"
+                size={75}
+                label="Toggle menu"
+              />
+            </div>
+            <ul
+              className={`${styles.navLinks} ${
+                isOpen ? styles.navLinksOpen : ""
+              }`}
+            >
+              <li>
+                <a href="#home" onClick={handleLinkClick}>
+                  <div className={styles.navList}>
+                    <Image
+                      src="/images/icons/home1.svg"
+                      alt="Home Icon"
+                      width={25}
+                      height={25}
+                    />
+                    <h1>Home</h1>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#about" onClick={handleLinkClick}>
+                  <div className={styles.navList}>
+                    <Image
+                      src="/images/icons/about1.svg"
+                      alt="About Icon"
+                      width={25}
+                      height={25}
+                    />
+                    <h1>About</h1>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#projects" onClick={handleLinkClick}>
+                  <div className={styles.navList}>
+                    <Image
+                      src="/images/icons/projects1.svg"
+                      alt="Projects Icon"
+                      width={25}
+                      height={25}
+                    />
+                    <h1>Projects</h1>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#contact" onClick={handleLinkClick}>
+                  <div className={styles.navList}>
+                    <Image
+                      src="/images/icons/contact1.svg"
+                      alt="Contact Icon"
+                      width={25}
+                      height={25}
+                    />
+                    <h1>Contact</h1>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-
-      {/* Mobile navigation container */}
-      <div className={styles.mobileNavBarContainer}>
-        <div className={styles.hamburgerContainer}>
-          <Hamburger
-            toggled={isOpen}
-            toggle={toggleMenu}
-            rounded
-            distance="md"
-            size={75}
-            label="Toggle menu"
-          />
-        </div>
-
-        {/* Navigation links list */}
-        <ul
-          className={`${styles.navLinks} ${isOpen ? styles.navLinksOpen : ""}`}
-        >
-          <li>
-            <a href="#home" onClick={toggleMenu}>
-              <div className={styles.navList}>
-                <Image
-                  src="/images/icons/home1.svg"
-                  alt="Home Icon"
-                  width={25}
-                  height={25}
-                />
-                <h1>Home</h1>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#about" onClick={toggleMenu}>
-              <div className={styles.navList}>
-                <Image
-                  src="/images/icons/about1.svg"
-                  alt="About Icon"
-                  width={25}
-                  height={25}
-                />
-                <h1>About</h1>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#projects" onClick={toggleMenu}>
-              <div className={styles.navList}>
-                <Image
-                  src="/images/icons/project1.svg"
-                  alt="Projects Icon"
-                  width={25}
-                  height={25}
-                />
-                <h1>Projects</h1>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#contact" onClick={toggleMenu}>
-              <div className={styles.navList}>
-                <Image
-                  src="/images/icons/contact1.svg"
-                  alt="Contact Icon"
-                  width={25}
-                  height={25}
-                />
-                <h1>Contact</h1>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    </>
   );
 };
 
